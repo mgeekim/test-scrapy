4883e61c8ad4241e1207290a42b447acf42e7653
mgeekim <64mingn@gmail.com>
2021-01-21 18:04:18 +0900

    Add bash script to set default envrionment

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt install python3.6 python3-pip python3.6-venv
+
+python3.6 -m venv venv
+source ./venv/bin/activate
+
+python3.6 -m pip install --upgrade pip
+python3.6 -m pip install -r requirements.txt
